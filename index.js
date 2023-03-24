b4bf4169793e46a9be64321485b230184be5f4a3
Jovan Stosic <jovanstosic012@gmail.com>
2023-03-24 15:34:22 -0500

    ADDED A COUNT DOWN TIMER, PLAYER LOSES IF TIMER HITS 0, TIMER RESETS WHEN PLAYER GETS ALIEN, PLAYER WINS WHEN THEY HIT 100 POINTS, AND OTHER MINOR CHANGES

--- index.js
+++ index.js
@@ -5,15 +5,20 @@ let invasionBtn = document.querySelector("#invasion");
 let score = document.querySelector("#score");
 let title = document.querySelector("#title");
 let section = document.querySelector("section");
+let timer = document.querySelector("#timer");
 
 //Creating new elements for use
 
+//creats a reload button to be set to reload the page when people win or fail
+let reload = document.createElement("button");
 //newScore is used to add points then set them equal to scores text
 let newScore = 0
 // used to create random number to move alien
 let mover;
 //used to save old number so no repeats and alien doesn't move each click
 let oldMover;
+//sets the time left for the user to get an alien
+let timeLeft = 5;
 
 // sets and creates invasion mode
 let invasion = false;
@@ -23,13 +28,23 @@ for (let i = 0; i < images.length; i++) {
     //this turns on a function when an image is clicked
   images[i].addEventListener("click", function() {
       console.log("you checked for aliens"); //for testing
-      // an if statement checks if it is an alien also checks if invasion was set on then sets score and moves alien
-      if (images[i].getAttribute("src") === "images/alienS.png" && !invasion) {
+      // An if statement checks if the score is 100 and tells the player they won!
+      if (newScore >=100) {
+        score.textContent = 'YOU WON!'
+        score.style.fontSize = '100px'
+        document.body.style.backgroundColor = 'gold';
+        score.append(reload);
+        reload.textContent = 'Play Again?'
+        timeLeft = 9999;
+        // an else if statement checks if it is an alien also checks if invasion was set on then sets score and moves alien
+        }else if (images[i].getAttribute("src") === "images/alienS.png" && !invasion) {
           console.log("you found the alien"); //for testing
           images[i].src = "images/space.png"
-          newScore++
+          // CHANGES THIS IF YOU WANNA CHEAT!!!!!!!
+          newScore ++
           score.textContent = newScore
           moveAlien()
+          timeLeft = 5;
           // invasion mode makes aliens swarm the user when one is clicked
         } else if (images[i].getAttribute("src") === "images/alienS.png" && invasion){
             console.log("you found the alien"); //for testing
@@ -38,6 +53,8 @@ for (let i = 0; i < images.length; i++) {
             score.textContent = newScore
             moveAlien()
             moveAlien()
+            // TIME IS LESS IN INVASION MODE
+            timeLeft = 3;
         }
     }); 
 }
@@ -56,6 +73,28 @@ function moveAlien() {
     }
 }
 
+//function that counts down if user doesn't get an alien they lose;
+const countdownTimer = setInterval(() => {
+  timer.textContent = `You have ${timeLeft} seconds to catch one`
+  timeLeft--;
+  if (timeLeft < 0) {
+    score.textContent = 'YOU FAILED'
+    score.style.fontSize = '100px'
+    document.body.style.backgroundColor = 'gray';
+    score.append(reload);
+    reload.textContent = 'Retry?'
+    images[0].src = "images/space.png"
+    images[1].src = "images/space.png"
+    images[2].src = "images/space.png"
+    images[3].src = "images/space.png"
+    images[4].src = "images/space.png"
+    images[5].src = "images/space.png"
+    images[6].src = "images/space.png"
+    images[7].src = "images/space.png"
+    images[8].src = "images/space.png"
+    clearInterval(countdownTimer);
+  }
+}, 1000);
 
 // The invasion button when clicked will turn on invasion mode changes colors and how aliens work see line 33
 invasionBtn.addEventListener("click", function(){
@@ -87,3 +126,8 @@ invasionBtn.addEventListener("click", function(){
     }
 })
 
+//reloads the page if you failed once the timer hit 0
+reload.addEventListener("click", function(){
+    // yes I had to googled this
+    window.location.reload();
+})
